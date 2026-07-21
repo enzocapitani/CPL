391a76a17bb91bc02387b463ff8edac44c981a5d
Enzo <enzocapitanivieira@gmail.com>
2026-07-21 18:50:03 -0300

    Feat: Criação do limpador de terminal

---
+++ src/CPL/util/CleanTerminal.java
@@ -0,0 +1,25 @@
+package CPL.util;
+
+public class CleanTerminal {
+	
+	/*
+	 * Esse método funciona apenas se o seu programa java está sendo rodado no terminal
+	 * em console de IDE costuma não funcionar
+	 */
+	public static void clean(){
+		
+        try {
+            String os = System.getProperty("os.name");
+
+            if(os.contains("Windows")){
+                new ProcessBuilder("cmd","/c","cls").inheritIO().start().waitFor();
+            }else{
+                new ProcessBuilder("clear").inheritIO().start().waitFor();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
