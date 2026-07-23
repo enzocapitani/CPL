0e1b11a440669f07e41c9e90100de6add74d4de3
Enzo <enzocapitanivieira@gmail.com>
2026-07-23 12:16:37 -0300

    Refactor(Menu): Agora o menu é desenhado conforme a maior string entre o titulo e as opções

--- src/CPL/menu/Menu.java
+++ src/CPL/menu/Menu.java
@@ -5,11 +5,11 @@ import CPL.style.Write;
 public class Menu {
     
     public static void simpleMenu(String title, String options[]){
-        String separator = Menu.generateSeparator(title.length());
+        String separator = Menu.generateSeparator(title, options);
 
         //Gera o titulo do menu :)
         System.out.println(separator);
-        System.out.println(Menu.generateCenterTitle(title));
+        System.out.println(Menu.generateCenterTitle(separator, title));
         System.out.println(separator);
 
         for(int i = 0; i < options.length; i++){
@@ -21,11 +21,11 @@ public class Menu {
     }
 
     public static void simpleTypeMenu(String title, String options[], int delay){
-        String separator = Menu.generateSeparator(title.length());
+        String separator = Menu.generateSeparator(title, options);
 
         //Gera o titulo do menu :)
         Write.write(separator, delay);
-        Write.write(Menu.generateCenterTitle(title), delay);
+        Write.write(Menu.generateCenterTitle(separator, title), delay);
         Write.write(separator, delay);
 
         for(int i = 0; i < options.length; i++){
@@ -36,23 +36,31 @@ public class Menu {
 
     }
 
-    public static String generateSeparator(int size){
-        int separatorLenght = size*3;
+    public static String generateSeparator(String title, String options[]){
+        int maiorString = title.length();
+
+        for(int i = 0; i < options.length; i++){
+            if(options[i].length() > maiorString) maiorString = options[i].length();
+        }
+
+        maiorString *= 2;
 
         StringBuilder sb = new StringBuilder();
 
-        for(int i = 0; i < separatorLenght; i++){
+        for(int i = 0; i < maiorString; i++){
             sb.append('=');
         }
 
         return sb.toString();
     }
 
-    public static String generateCenterTitle(String title){
+    public static String generateCenterTitle(String separator, String title){
 
         StringBuilder sb = new StringBuilder();
 
-        for(int i = 0; i < title.length(); i++){
+        int espacoBranco = separator.length()/2 - title.length()/2;
+
+        for(int i = 0; i < espacoBranco; i++){
             sb.append(" ");
         }
 
